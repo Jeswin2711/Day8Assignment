6ccf35c0d66ca0a1a207493386265f8ddafbbec2
Jeswin2711 <jeswin2711@gmail.com>
2022-07-18 14:50:33 +0530

    Added:UseCase1

--- src/main/java/com/assignment/EmployeeWages.java
+++ src/main/java/com/assignment/EmployeeWages.java
@@ -1,11 +1,16 @@
 package com.assignment;
 
-public class EmployeeWages
+public class EmployeeWages extends EmployeeAttendance
 {
     /*
     Main Class to create Objects and access Class function
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Welcome To Employee Wage Computation Program");
+        System.out.println("--------------------------------------------");
+        EmployeeAttendance employee = new EmployeeAttendance();
+        int employeeAttendance = employee.getAttendance();
+
     }
 }
