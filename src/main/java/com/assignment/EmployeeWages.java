a40e4fba598de6e22c4eff62eacb7bb981564469
Jeswin2711 <jeswin2711@gmail.com>
2022-07-18 14:36:48 +0530

    Added:Welcome

---
+++ src/main/java/com/assignment/EmployeeWages.java
@@ -0,0 +1,11 @@
+package com.assignment;
+
+public class EmployeeWages
+{
+    /*
+    Main Class to create Objects and access Class function
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee Wage Computation Program");
+    }
+}
